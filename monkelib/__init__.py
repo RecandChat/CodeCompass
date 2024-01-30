9e10dfb189e6b94620b20f96bf7c0a2c0cf4818b
Lukasaurus11 <lucacuneob@gmail.com>
2024-01-30 15:55:53 +0100

    Added new helper function
    Added new helper function to remove code redundancy

--- monkelib/__init__.py
+++ monkelib/__init__.py
@@ -3,7 +3,7 @@ from API import get_data
 
 def main():
     # Making sure code works
-    if get_data.get_user_repo("felixggj"):
+    if get_data.get_user_repo("Lukasaurus11"):
         print("Success!")
     else:
         print("Failure!")
