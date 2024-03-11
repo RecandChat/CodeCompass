46cb171901126379a856e287019d179b861f7d7e
maudhelen <mhovland.ieu2021@student.ie.edu>
2024-03-11 14:02:19 +0000

    intial connection

--- drive.py
+++ drive.py
@@ -1,7 +1,10 @@
 from pydrive.auth import GoogleAuth 
 from pydrive.drive import GoogleDrive 
 
-gauth = GoogleAuth() #Default file is client_secrets.json
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
 drive = GoogleDrive(gauth)
 
 path = "https://drive.google.com/drive/folders/13JitBJQLNgMvFwx4QJcvrmDwKOYAShVx"
@@ -9,6 +12,9 @@ path = "https://drive.google.com/drive/folders/13JitBJQLNgMvFwx4QJcvrmDwKOYAShVx
 folder_id = path.split('/')[-1]
 
 file_list = drive.ListFile({'q': "'{}' in parents and trashed=false".format(folder_id)}).GetList()
+
+if file_list == []:
+  print('No files found.')
 
 for file in file_list:
     print('title: %s, id: %s' % (file['title'], file['id']))
