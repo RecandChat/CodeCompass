d84e0cb559f0a77a886c4b4766c0ee103f335758
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2024-04-01 23:30:46 +0200

    Created the Demo gpt-4 assistant

--- codecompasslib/chatbot/main.py
+++ codecompasslib/chatbot/main.py
@@ -11,18 +11,20 @@ github_token = load_github_token(file_path='./secrets/github_token')
 # Initialize the OpenAI client
 client = initialize_client(openAI_key)
 
-# Create an assistant
+# Create an assistant (This should not be used if the assistant already exists)
 """
 instructions = load_assistant_instructions(file_path='secrets/instructions')
 new_assistant = create_assistant(
     client=client,
     name="CodeCompass",
     instructions=instructions,
-    model="gpt-3.5-turbo-0125",
-    tools=load_tools('chatbot/tools.json'))
+    model="gpt-4-0125-preview",
+    tools=load_tools('codecompasslib/chatbot/tools.json'))
 """
+# Retrieve assistant using GPT-4 (This should be used for final demo)
+#assistant = retrieve_assistant(client, 'asst_lfVLtdNac9aVUvDIIlw5c1nY')
 
-# Retrieve assistant
+# Retrieve assistant using GPT-3.5
 assistant = retrieve_assistant(client, 'asst_cibfhJsvFEKbAo7EmaS34oQD')
 
 # Run the chatbot
